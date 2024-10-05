5d21a5ff31d9b36a237f4712019fa0bb9589c2ed
PiotrHic <piotrhic@gmail.com>
2024-10-05 20:50:55 +0200

    Create TDD for Dancer Class. Closes #7.

---
+++ src/test/java/com/example/dance_school_jpa_app/domain/DancerTest.java
@@ -0,0 +1,28 @@
+package com.example.dance_school_jpa_app.domain;
+
+import org.junit.jupiter.api.Test;
+
+public class DancerTest {
+
+    Dancer dancer = Dancer.builder().build();
+
+    @Test
+    public void setId(){
+
+    }
+
+    @Test
+    public void getId(){
+
+    }
+
+    @Test
+    public void setName(){
+
+    }
+
+    @Test
+    public void getName(){
+
+    }
+}
