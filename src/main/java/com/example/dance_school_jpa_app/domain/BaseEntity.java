9ae77242619d310408a98711816530fb468343d4
PiotrHic <piotrhic@gmail.com>
2024-10-05 20:35:47 +0200

    Create BaseEntityClass. Closes #5.

---
+++ src/main/java/com/example/dance_school_jpa_app/domain/BaseEntity.java
@@ -0,0 +1,32 @@
+package com.example.dance_school_jpa_app.domain;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.MappedSuperclass;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@SuperBuilder
+@MappedSuperclass
+public class BaseEntity {
+
+    @Column(
+            updatable = false,
+            nullable = false)
+    private LocalDateTime createdAt;
+    @Column(
+            insertable = false
+    )
+    private LocalDateTime lastModifiedAt;
+    private String createdBy;
+    private String lastModifiedBy;
+}
