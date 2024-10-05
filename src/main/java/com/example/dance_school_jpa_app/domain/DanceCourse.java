e91d1cd9a46b5220fae886738426f24a6b43c880
PiotrHic <piotrhic@gmail.com>
2024-10-05 21:18:50 +0200

    Execute Dancer Course test. Closes #12.

---
+++ src/main/java/com/example/dance_school_jpa_app/domain/DanceCourse.java
@@ -0,0 +1,27 @@
+package com.example.dance_school_jpa_app.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@SuperBuilder
+@Table(name = "Dance_Course_Table")
+@Entity
+public class DanceCourse {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "Dance_Course_Table_generator")
+    @SequenceGenerator(name = "Dance_Course_Table_generator", sequenceName = "Dance_Course_Table_generator")
+    @Column(name = "dance_course_id", nullable = false)
+    private Integer id;
+
+    @Column(name = "dance_course_name", nullable = false, unique = true)
+    private String name;
+
+}
--- src/test/java/com/example/dance_school_jpa_app/domain/DanceCourseTest.java
+++ src/test/java/com/example/dance_school_jpa_app/domain/DanceCourseTest.java
@@ -1,28 +1,51 @@
 package com.example.dance_school_jpa_app.domain;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class DanceCourseTest {
 
-//    DanceCourse dancerCourse = DanceCourse.builder().build();
+    DanceCourse dancerCourse = DanceCourse.builder()
+            .id(1)
+            .name("test").build();
     @Test
     public void setId(){
-
+        //given
+        Integer expected = 10;
+        //when
+        dancerCourse.setId(expected);
+        //then
+        Assertions.assertEquals(expected, dancerCourse.getId());
     }
 
     @Test
     public void getId(){
-
+        //given
+        Integer expected = 10;
+        //when
+        dancerCourse.setId(expected);
+        //then
+        Assertions.assertEquals(expected, dancerCourse.getId());
     }
 
     @Test
     public void setName(){
-
+        //given
+        String expected = "name";
+        //when
+        dancerCourse.setName(expected);
+        //then
+        Assertions.assertEquals(expected, dancerCourse.getName());
     }
 
     @Test
     public void getName(){
-
+        //given
+        String expected = "name";
+        //when
+        dancerCourse.setName(expected);
+        //then
+        Assertions.assertEquals(expected, dancerCourse.getName());
     }
 
 }
