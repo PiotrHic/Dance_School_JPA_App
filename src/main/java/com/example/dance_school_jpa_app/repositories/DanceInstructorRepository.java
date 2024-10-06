ce519600e72194479efabb46936b7bcd1fe5eac7
PiotrHic <piotrhic@gmail.com>
2024-10-06 13:15:32 +0200

    Create Dance Instructor Repository. Closes #25.

---
+++ src/main/java/com/example/dance_school_jpa_app/repositories/DanceInstructorRepository.java
@@ -0,0 +1,7 @@
+package com.example.dance_school_jpa_app.repositories;
+
+import com.example.dance_school_jpa_app.domain.DanceInstructor;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DanceInstructorRepository extends JpaRepository<DanceInstructor,Integer> {
+}
