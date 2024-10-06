834b2d2540d9cf42eaf1f7a64ea8589739784b59
PiotrHic <piotrhic@gmail.com>
2024-10-06 22:21:43 +0200

    Create TDD for Dance Course Service. Closes #39.

---
+++ src/test/java/com/example/dance_school_jpa_app/service/DanceCourseServiceMockTest.java
@@ -0,0 +1,134 @@
+package com.example.dance_school_jpa_app.service;
+
+import com.example.dance_school_jpa_app.domain.DanceCourse;
+import com.example.dance_school_jpa_app.domain.Dancer;
+import com.example.dance_school_jpa_app.repositories.DanceCourseRepository;
+import com.example.dance_school_jpa_app.repositories.DancerRepository;
+import com.example.dance_school_jpa_app.services.DancerServiceImpl;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class DanceCourseServiceMockTest {
+
+    @Mock
+    DanceCourseRepository danceCourseRepository;
+
+    AutoCloseable autoCloseable;
+    @InjectMocks
+    DanceCourseServiceImpl danceCourseService;
+
+    DanceCourse returned = DanceCourse.builder()
+            .name("test")
+            .createdAt(LocalDateTime.now())
+            .lastModifiedAt(LocalDateTime.now())
+            .createdBy("test1")
+            .lastModifiedBy("test1")
+            .build();
+
+    DanceCourse first = DanceCourse.builder()
+            .name("test1")
+            .createdAt(LocalDateTime.now())
+            .lastModifiedAt(LocalDateTime.now())
+            .createdBy("test")
+            .lastModifiedBy("test")
+            .build();
+
+    DanceCourse second = DanceCourse.builder()
+            .name("test2")
+            .createdAt(LocalDateTime.now())
+            .lastModifiedAt(LocalDateTime.now())
+            .createdBy("test2")
+            .lastModifiedBy("test2")
+            .build();
+
+
+    @BeforeEach
+    void setUp(){
+        autoCloseable = MockitoAnnotations.openMocks(this);
+        danceCourseService = new DancerServiceImpl(danceCourseRepository);
+    }
+
+    @AfterEach
+    void tearDown() throws Exception{
+        autoCloseable.close();
+    }
+
+    @Test
+    void testMock(){
+        Map mapMock = mock(Map.class);
+
+        assertThat(mapMock.size()).isEqualTo(0);
+    }
+
+
+    @Test
+    void createDanceCourse (){
+
+        //stub the data
+        when(danceCourseRepository.save(returned)).thenReturn(returned);
+
+        Dancer result = danceCourseService.createDancer(returned);
+        Assertions.assertEquals("test", result.getName());
+    }
+
+    @Disabled
+    @Test
+    public void getOneDanceCourse() throws Exception{
+
+        //stub the data
+        when(danceCourseRepository.getReferenceById(returned.getId())).thenReturn(returned);
+        Dancer result = danceCourseService.getDancer(returned.getId());
+        Assertions.assertEquals("test", result.getName());
+    }
+
+    @Test
+    void getAllDanceCourse(){
+        // when
+        when(danceCourseRepository.findAll()).
+                thenReturn(Arrays.asList(first,
+                        second));
+
+        //then
+        List<Dancer> dancers = danceCourseService.getAllDancers();
+        Assertions.assertEquals(dancers.size(), 2);
+    }
+
+    @Disabled
+    @Test
+    void updateDanceCourse(){
+
+
+        //stub the data
+        when(danceCourseRepository.getReferenceById(first.getId())).thenReturn(first);
+        when(danceCourseRepository.save(first)).thenReturn(first);
+
+        danceCourseService.createDancer(first);
+
+        Dancer result = danceCourseService.updateDancer(second.getId(), second);
+        Assertions.assertEquals("test2", result.getName());
+
+    }
+
+    @Disabled
+    @Test
+    void deleteDanceCourse(){
+
+        when(danceCourseRepository.getReferenceById(returned.getId())).thenReturn(returned);
+        Dancer result = danceCourseService.deleteDancer(returned.getId());
+        Assertions.assertEquals("test", result.getName());
+    }
+}
